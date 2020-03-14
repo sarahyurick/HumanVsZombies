0398d0ee68e98bda80b896e5b5ef9d5679633425
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2020-03-14 12:50:33 -0400

    Add files via upload

---
+++ Tutorial/2/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float speed = 1.5f;
+
+    // Start is called before the first frame update
+    /*
+    void Start()
+    {
+
+    }
+    */
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        transform.position = transform.position + movement * Time.deltaTime;
+
+        /*
+        // wasd version
+        Vector3 pos = transform.position;
+        if (Input.GetKey ("w")) { pos.y += speed * Time.deltaTime; }
+        if (Input.GetKey ("s")) { pos.y -= speed * Time.deltaTime; }
+        if (Input.GetKey ("d")) { pos.x += speed * Time.deltaTime; }
+        if (Input.GetKey ("a")) { pos.x -= speed * Time.deltaTime; }
+        transform.position = pos;
+        */
+
+    }
+}
