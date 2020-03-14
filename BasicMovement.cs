e0a3ddf7e34358e23e2c44bf70f28465c601c956
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2020-03-14 01:27:34 -0400

    BasicMovement to be used for first tutorial

--- BasicMovement.cs
+++ BasicMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,15 +7,25 @@ public class BasicMovement : MonoBehaviour
     public float speed = 1.5f;
 
     // Start is called before the first frame update
+    /*
     void Start()
     {
 
     }
+    */
+
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
 
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+
+        /*
         Vector3 arrow = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
         arrow = speed * arrow * Time.deltaTime;
         transform.position = transform.position + arrow;
@@ -27,6 +37,7 @@ public class BasicMovement : MonoBehaviour
         if (Input.GetKey ("d")) { pos.x += speed * Time.deltaTime; }
         if (Input.GetKey ("a")) { pos.x -= speed * Time.deltaTime; }
         transform.position = pos;
+        */
 
     }
 }
