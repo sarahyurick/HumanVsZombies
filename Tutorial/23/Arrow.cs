f2e9f7d2ed565762d5820702e08e688b136065d2
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2020-03-22 17:42:55 -0400

    Create Arrow.cs

---
+++ Tutorial/23/Arrow.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    public Vector2 velocity = new Vector2(0.0f, 0.0f);
+    public GameObject archer;
+    public Vector2 offset = new Vector2(0.0f, 0.0f);
+
+    void Update()
+    {
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 newPosition = currentPosition + velocity * Time.deltaTime;
+
+        Debug.DrawLine(currentPosition + offset, newPosition + offset, Color.red);
+
+        RaycastHit2D[] hits = Physics2D.LinecastAll(currentPosition + offset, newPosition + offset);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            GameObject other = hit.collider.gameObject;
+            if(other != archer)
+            {
+                if(other.CompareTag("Zombies"))
+                {
+                    HvZEvents.zombieHit.Invoke(new HitEventData(archer, other, gameObject));
+                    Destroy(gameObject);
+                    Destroy(other);
+                    Debug.Log(other.name);
+                    // Debug.Log(hit.collider.gameObject);
+                    break;
+                }
+
+                if(other.CompareTag("Trees"))
+                {
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+
+        transform.position = newPosition;
+    }
+}
