eb67c0b5070481d8f2acd2ca843bd446dfd1ca76
Sarah Yurick <sey13@case.edu>
2020-04-14 13:19:20 -0400

    Ready for functional testing demo

--- 393 Demo 1/Assets/Scripts/UIScripts/MusicClass.cs
+++ 393 Demo 1/Assets/Scripts/UIScripts/MusicClass.cs
@@ -7,16 +7,47 @@ public class MusicClass : MonoBehaviour
     // Used https://answers.unity.com/questions/1260393/make-music-continue-playing-through-scenes.html
 
     public AudioSource _audioSource;
+    public bool newGame;
+    public Toggle MusicToggle;
+    public Toggle SoundToggle;
 
     private void Awake()
     {
         // DontDestroyOnLoad(transform.gameObject);
-        _audioSource = GetComponent<AudioSource>();
+        // _audioSource = GetComponent<AudioSource>();
+        if(newGame)
+        {
+            PlayerPrefs.SetInt("Music", 1);
+            PlayerPrefs.SetInt("Sound", 1);
+        }
+    }
+
+    public void Update()
+    {
+        /*
+        if(PlayerPrefs.GetInt("Music") == 1 && MusicToggle.isOn == false)
+        {
+            MusicToggle.isOn = true;
+        }
+        if (PlayerPrefs.GetInt("Music") == 0 && MusicToggle.isOn == true)
+        {
+            MusicToggle.isOn = false;
+        }
+        Debug.Log(PlayerPrefs.GetInt("Music") + " " + MusicToggle.isOn);
+
+        if (PlayerPrefs.GetInt("Sound") == 1 && SoundToggle.isOn == false)
+        {
+            SoundToggle.isOn = true;
+        }
+        if (PlayerPrefs.GetInt("Sound") == 0 && SoundToggle.isOn == true)
+        {
+            SoundToggle.isOn = false;
+        }*/
     }
 
     public void PlayMusic()
     {
-        if(PlayerPrefs.GetInt("Music", 0) == 1)
+        if(PlayerPrefs.GetInt("Music") == 1)
         {
             if (_audioSource.isPlaying) return;
             _audioSource.Play();
@@ -33,7 +64,7 @@ public class MusicClass : MonoBehaviour
 
     public void ToggleMusic()
     {
-        if (PlayerPrefs.GetInt("Music", 0) == 1)
+        if (PlayerPrefs.GetInt("Music") == 1)
         {
             PlayerPrefs.SetInt("Music", 0);
             StopMusic();
@@ -42,11 +73,12 @@ public class MusicClass : MonoBehaviour
             PlayerPrefs.SetInt("Music", 1);
             PlayMusic();
         }
+        PlayerPrefs.Save();
     }
 
     public void ToggleSound()
     {
-        if (PlayerPrefs.GetInt("Sound", 0) == 1)
+        if (PlayerPrefs.GetInt("Sound") == 1)
         {
             PlayerPrefs.SetInt("Sound", 0);
         }
@@ -54,5 +86,6 @@ public class MusicClass : MonoBehaviour
         {
             PlayerPrefs.SetInt("Sound", 1);
         }
+        PlayerPrefs.Save();
     }
 }
